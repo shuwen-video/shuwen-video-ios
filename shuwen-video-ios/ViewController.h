6ebea31378ff04eb123552df42c47818d55bd9db
ShuwenHe <1201220707@pku.edu.cn>
2025-01-16 10:18:45 +0800

    feat:shuwen video ios

---
+++ shuwen-video-ios/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  shuwen-video-ios
+//
+//  Created by shuwen on 2025/1/16.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ shuwen-video-ios/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  shuwen-video-ios
+//
+//  Created by shuwen on 2025/1/16.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
